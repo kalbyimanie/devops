b94e9778b0062cd0769547fee05ac8856444aafd
kalbyimanie <aby.imanie@gmail>
2021-08-16 19:43:23 +0700

    added packer files

---
+++ packer/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update -y && \
+
+# install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y && \
+
+curl -fsSL get.docker.com | sh && \
+sudo apt-get update -y && \
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y && \
+sudo systemctl status docker
+# end of install docker
