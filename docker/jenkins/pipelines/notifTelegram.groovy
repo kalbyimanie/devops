cd75a568d469b4f54eb1e05bb0c96f5fac53ee5d
kalbyimanie <aby.imanie@gmail>
2021-11-20 21:53:07 +0700

    modified pipeline

--- docker/jenkins/pipelines/notifTelegram.groovy
+++ docker/jenkins/pipelines/notifTelegram.groovy
@@ -10,16 +10,16 @@ timestamps {
 }
 
 def sendTelegram(message) {
-    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+  def encodedMessage = URLEncoder.encode(message, "UTF-8")
 
-    withCredentials([string(credentialsId: 'telegramToken', variable: 'TOKEN'),
-    string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')]) {
+  withCredentials([string(credentialsId: 'telegramToken', variable: 'TOKEN'),
+  string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')]) {
 
-        response = httpRequest (consoleLogResponseBody: true,
-                contentType: 'APPLICATION_JSON',
-                httpMode: 'GET',
-                url: "https://api.telegram.org/bot$TOKEN/sendMessage?text=$encodedMessage&chat_id=$CHAT_ID&disable_web_page_preview=true",
-                validResponseCodes: '200')
-        return response
-    }
+      response = httpRequest (consoleLogResponseBody: true,
+              contentType: 'APPLICATION_JSON',
+              httpMode: 'GET',
+              url: "https://api.telegram.org/bot$TOKEN/sendMessage?text=$encodedMessage&chat_id=$CHAT_ID&disable_web_page_preview=true",
+              validResponseCodes: '200')
+      return response
+  }
 }
