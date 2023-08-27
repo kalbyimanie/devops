56fc34f4d654004811e40ed2bf54a26bd99e38b3
kalbyimanie <aby.imanie@gmail>
2023-08-28 02:00:34 +0700

    changes

--- docker/jenkins/scripts/copy_jenkins_env.sh
+++ docker/jenkins/scripts/copy_jenkins_env.sh
@@ -6,12 +6,26 @@ JENKINS_DECRYPTED_FILE="$JENKINS_DECRYPTED_FILE_PATH/jenkins.env.dec"
 DESTINATION_FILE_PATH="../casc-configs"
 JENKINS_ENV_FILE="$DESTINATION_FILE_PATH/jenkins.env"
 
-if [[ ! -f "$JENKINS_DECRYPTED_FILE" ]];then
-    echo -en "ERROR: $JENKINS_DECRYPTED_FILE does not exist\n"
-    echo -en "Please prepare the file\n"
+FLAG=""
 
-    exit 1
+if [[ -f "$JENKINS_ENV_FILE" ]]; then
+    FLAG="exists"
 fi
-echo -ne "Copying jenkins env file...\n"
-# cd $DESTINATION_FILE_PATH
-cp -pv $JENKINS_DECRYPTED_FILE $JENKINS_ENV_FILE
+
+case "$FLAG" in
+    exists)
+        echo "Using existing jenkins env file"
+    ;;
+
+    *)
+        if [[ ! -f "$JENKINS_DECRYPTED_FILE" ]];then
+            echo -en "ERROR: $JENKINS_DECRYPTED_FILE does not exist\n"
+            echo -en "Please prepare the file\n"
+
+            exit 1
+        fi
+        echo -ne "Copying jenkins env file from $JENKINS_DECRYPTED_FILE...\n"
+        cp -pv $JENKINS_DECRYPTED_FILE $JENKINS_ENV_FILE
+    ;;
+esac
+
