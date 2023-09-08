5be69290021f9df3c6690f20e0e79a60ad930849
kalbyimanie <aby.imanie@gmail>
2023-09-09 03:07:19 +0700

    added share lib

--- docker/jenkins/pipelines/shared-library/vars/helloWorld.groovy
+++ docker/jenkins/pipelines/shared-library/vars/helloWorld.groovy
@@ -1,44 +1,14 @@
 def call(Map args) {
     // !!! IMPORTANT !!! SECTION variables
-    def hello = args.hello
+    def param1 = args.param1
+    def param2 = args.param2
 
     // !!! IMPORTANT !!! SECTION pipeline
     pipeline {
-
-        // options {
-
-        // }
-
-        // agent {
-        //     docker {
-        //         yaml """
-        //             """
-        //     }
-        // }
         stage ('FROM SHARED LIBRARY') {
             sh """
-                 echo "${hello}"
-               """
-
-            // steps {
-            //     script {
-            //         """
-            //             sh "echo ${hello}"
-            //         """
-            //     }
-            // }
+                echo "${hello} ${world}"
+            """
         }
-
-        // stages {
-        //     stage ('[shared library]—init stage') {
-        //         steps {
-        //             script {
-        //                 """
-        //                     sh "echo ${hello}"
-        //                 """
-        //             }
-        //         }
-        //     }
-        // }
     }
 }
